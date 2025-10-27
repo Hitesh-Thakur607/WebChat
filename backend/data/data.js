3f2b0bafa81205c4ca70a044f16ab9f0b5e09623
hitesh <hitesht130@gmail.com>
2025-10-27 12:30:56 +0530

    old commit

--- backend/data/data.js
+++ backend/data/data.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const connectdb=()=>{
-    mongoose.connect("mongodb+srv://hitesht130_db_user:6c2YZNS70ZhKKSW7@hitesht169.cfwn7zq.mongodb.net/?retryWrites=true&w=majority&appName=hitesht169",{
+    mongoose.connect("mongodb+srv://hitesht130:102030@cluster0.kipkqzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
         dbName:"webchat",
     })
     .then((c)=>console.log("MongoDB connected",c.connection.host))
